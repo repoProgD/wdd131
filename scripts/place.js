c36e29cddb448e664d1696dc6854c1dba768fcbc
unknown <diegoprogra28@gmail.com>
2026-01-24 15:20:09 -0300

    w03 assigment 004, windchill function modified

--- scripts/place.js
+++ scripts/place.js
@@ -1,28 +1,25 @@
 /*°F*/
-const calculateWindChill = (t, w) => {
-    if (t <= 50 && w > 3) {
-        const chill = 35.74 + (0.6215 * t) - (35.75 * Math.pow(w, 0.16)) + (0.4275 * t * Math.pow(w, 0.16));
-        return `${chill.toFixed(1)}°F / ${farenheitToCelsius(chill)}°C`;
-    }
+const calculateWindChill = (t, w) =>  35.74 + (0.6215 * t) - (35.75 * Math.pow(w, 0.16)) + (0.4275 * t * Math.pow(w, 0.16));
+     
+const farenheitToCelsius = (f) => ((f - 32) * (5 / 9)).toFixed(1);
 
-    else {
-        return "N/A";
-    }
-};
-
-const farenheitToCelsius = (f) => {
-    return ((f - 32) * 5 / 9).toFixed(1);
-}
 
 document.addEventListener("DOMContentLoaded", () => {
     const tempElement = document.querySelector("#temp");
     const windElement = document.querySelector("#wind");
     const windChillElement = document.querySelector("#wind-chill");
 
-    if (tempElement && windElement && windChillElement) { 
-        const temp = parseFloat(tempElement.textContent.split("/")[0]);
-        const windSpeed = parseFloat(windElement.textContent);
-        windChillElement.textContent = calculateWindChill(temp, windSpeed);
+    const temp = parseFloat(tempElement.textContent.split("/")[0]);
+    const windSpeed = parseFloat(windElement.textContent);
+
+    if (temp <= 50 && windSpeed > 3) {
+        
+        const chillFar = calculateWindChill(temp, windSpeed);
+        windChillElement.textContent = `${chillFar.toFixed(1)}°F / ${farenheitToCelsius(chillFar)}°C`;
+    }
+
+    else { 
+        windChillElement.textContent = "N/A";
     }
 
 });
